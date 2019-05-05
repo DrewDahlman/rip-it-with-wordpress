fb50386ecadf22927b7b93e79798bbb734e20777
Drew Dahlman <info@drewdahlman.com>
2019-05-05 09:03:08 -0400

    update footer to remove jQuery and WPEmbed

--- src/markup/functions.php
+++ src/markup/functions.php
@@ -1,3 +1,12 @@
 <?php
-	// if ( !is_admin() ) wp_deregister_script('jquery');
+	
+	// Remove WpEmbed & jQuery
+	function removeWpEmbedAndjQuery() {
+		if (!is_admin()) {
+			wp_deregister_script('wp-embed');
+			wp_deregister_script('jquery');  // Bonus: remove jquery too if it's not required
+		}
+	}
+	add_action('init', 'removeWpEmbedAndjQuery');
+	
 ?>
